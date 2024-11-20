ae0818832e3723957c173c625fa9ca2c5ec93018
Sarah Simionescu <sarah.simionescu@gmail.com>
2024-11-20 11:46:01 -0500

    refactor: replace discord auth with github auth

--- src/frontend/src/server/auth/config.ts
+++ src/frontend/src/server/auth/config.ts
@@ -1,6 +1,5 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { db } from "~/server/db";
 import {
@@ -9,6 +8,8 @@ import {
   users,
   verificationTokens,
 } from "~/server/db/schema";
+import GithubProvider from "next-auth/providers/github";
+import { env } from "~/env";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -38,16 +39,10 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    GithubProvider({
+      clientId: env.AUTH_GITHUB_ID,
+      clientSecret: env.AUTH_GITHUB_SECRET,
+    }),
   ],
   adapter: DrizzleAdapter(db, {
     usersTable: users,
