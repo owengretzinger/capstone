8f08cd362a2cda2b80021acd78ce801e9a0c57ed
Sarah Simionescu <sarah.simionescu@gmail.com>
2025-01-07 13:52:10 -0500

    feat: Create Endpoint to Download Audio

--- src/backend/app/routes/bots.py
+++ src/backend/app/routes/bots.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from app.models import Bot, MeetingInfo
 from app.models import BotCreate
-
+import boto3
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.db import get_session
 
 router = APIRouter()
-
+client = boto3.client('s3')
 
 @router.post("/create_bot", response_model=Bot)
 async def create_bot(data: BotCreate, session: AsyncSession = Depends(get_session)):
@@ -41,4 +41,16 @@ async def get_bot(bot_id: int, session: AsyncSession = Depends(get_session)):
     bot = result.first()
     if not bot:
         raise HTTPException(status_code=404, detail="Bot not found")
+    
+    key = bot.audio_s3_object_key
+
+    audio_recording_url = client.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': 'your-bucket-name', 'Key': key},
+        ExpiresIn=3600
+    )
+
+    bot.audio_recording_url = audio_recording_url # add the url to the bot object
+    del bot.audio_s3_object_key # remove the key from the bot object
+
     return bot
